4416750770fcbc18b9608c7dd57a15dc31dc9c6e
tasadar2 <tassadar@wowway.com>
2017-10-26 16:24:54 -0400

    Changed path to local interfaces path

--- lib/handlers/personalaccesstoken.ts
+++ lib/handlers/personalaccesstoken.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import VsoBaseInterfaces = require('../interfaces/common/VsoBaseInterfaces');
+import VsoBaseInterfaces = require('../interfaces');
 
 export class PersonalAccessTokenCredentialHandler implements VsoBaseInterfaces.IRequestHandler {
     token: string;
