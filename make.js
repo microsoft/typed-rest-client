7c570b03a9f6bbc59249a75326033f8ba72bfb81
Stephen Franceschelli <stephenmichaelf@gmail.com>
2017-12-07 11:57:01 -0500

    Enforce node and npm versions in make.js (#36)

--- make.js
+++ make.js
@@ -1,13 +1,35 @@
 require('shelljs/make');
 var path = require('path');
 var fs = require('fs');
+var semver = require('semver');
+var ncp = require('child_process');
 
 var rp = function (relPath) {
     return path.join(__dirname, relPath);
 }
 
+var fail = function (message) {
+    console.error('ERROR: ' + message);
+    process.exit(1);
+}
+
 var buildPath = path.join(__dirname, '_build');
 var testPath = path.join(__dirname, 'test');
+
+// enforce minimum Node version
+var minimumNodeVersion = '4.8.6';
+var currentNodeVersion = process.versions.node;
+if (semver.lt(currentNodeVersion, minimumNodeVersion)) {
+    fail('requires node >= ' + minimumNodeVersion + '.  installed: ' + currentNodeVersion);
+}
+
+// enforce minimum npm version
+// NOTE: We are enforcing this version of npm because we use package-lock.json
+var minimumNpmVersion = '5.5.1';
+var currentNpmVersion = ncp.execSync('npm -v', { encoding: 'utf-8' });
+if (semver.lt(currentNpmVersion, minimumNpmVersion)) {
+    fail('requires npm >= ' + minimumNpmVersion + '.  installed: ' + currentNpmVersion);
+}
 
 var run = function (cl) {
     try {
@@ -29,8 +51,6 @@ target.clean = function () {
 };
 
 target.build = function () {
-    target.clean();
-
     run(path.join(__dirname, 'node_modules/.bin/tsc') + ' --outDir ' + buildPath);
     cp('-Rf', rp('lib/opensource'), buildPath);
     cp(rp('package.json'), buildPath);
@@ -39,8 +59,6 @@ target.build = function () {
 }
 
 target.test = function() {
-    target.build();
-
     // install the just built lib into the test proj
     pushd('test')
     run('npm install ../_build');
@@ -56,8 +74,6 @@ target.buildtest = function() {
 }
 
 target.samples = function () {
-    target.build();
-
     pushd('samples');
     run('npm install ../_build');
     run('tsc');
