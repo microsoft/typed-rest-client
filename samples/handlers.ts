42c516f409b0868a2f1b57ecb5646c6443fd0f82
Stephen Franceschelli <Stephen.Franceschelli@microsoft.com>
2017-12-12 15:23:43 -0500

    Add NTLM authorization handler.

--- samples/handlers.ts
+++ samples/handlers.ts
@@ -1,15 +1,23 @@
 import * as hm from 'typed-rest-client/Handlers'
 import * as cm from './common';
+import * as httpm from 'typed-rest-client/HttpClient';
 
 export async function run() {
     cm.banner('Handler Samples');
+
+    var username = "";
+    var password = "";
+    var workstation = "";
+    var domain = "";
+    var url = "";
     
-    let bh: hm.BasicCredentialHandler = new hm.BasicCredentialHandler('johndoe', 'password');
-    let ph: hm.PersonalAccessTokenCredentialHandler = 
-        new hm.PersonalAccessTokenCredentialHandler('scbfb44vxzku5l4xgc3qfazn3lpk4awflfryc76esaiq7aypcbhs');
-    let nh: hm.NtlmCredentialHandler = new hm.NtlmCredentialHandler('johndoe', 'password');
+    const basicHandler: hm.BasicCredentialHandler = new hm.BasicCredentialHandler(username, password);
+    const patHandler: hm.PersonalAccessTokenCredentialHandler = new hm.PersonalAccessTokenCredentialHandler('scbfb44vxzku5l4xgc3qfazn3lpk4awflfryc76esaiq7aypcbhs');
+    const ntlmHandler: hm.NtlmCredentialHandler = new hm.NtlmCredentialHandler(username, password, workstation, domain);
+
     // These handlers would then be passed to the constructors of the http or rest modules
+
+    // const httpClient: httpm.HttpClient = new httpm.HttpClient('vsts-node-api', [ntlmHandler]);
+    // const response: httpm.HttpClientResponse = await httpClient.get(url);
+    // console.log("response code: " + response.message.statusCode);
 }
-
-
-
