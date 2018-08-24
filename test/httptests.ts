2366e11aacf75a4b4f0e22c11cf9d45786725c90
William <william.bernardet@gmail.com>
2018-08-23 19:53:38 -0700

    Handle 303 SeeOther redirect. (#75)
    * Handle 303 SeeOther redirect.
    
    * Add unittest to covert 404 on redirect.

--- test/httptests.ts
+++ test/httptests.ts
@@ -101,14 +101,28 @@ describe('Http Tests', function () {
         let obj:any = JSON.parse(body);
         assert(obj.url === "https://httpbin.org/get");
     });
-    
+
+    it('does basic get request with redirects (303)', async() => {
+        let res: httpm.HttpClientResponse = await _http.get("https://httpbin.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/get") + '&status_code=303')
+        assert(res.message.statusCode == 200, "status code should be 200");
+        let body: string = await res.readBody();
+        let obj:any = JSON.parse(body);
+        assert(obj.url === "https://httpbin.org/get");
+    });
+
+    it('returns 404 for not found get request on redirect', async() => {
+        let res: httpm.HttpClientResponse = await _http.get("https://httpbin.org/redirect-to?url=" + encodeURIComponent("http://httpbin.org/status/404") + '&status_code=303')
+        assert(res.message.statusCode == 404, "status code should be 404");
+        let body: string = await res.readBody();
+    });
+
     it('does not follow redirects if disabled', async() => {
         let http: httpm.HttpClient = new httpm.HttpClient('typed-test-client-tests', null, { allowRedirects: false });
         let res: httpm.HttpClientResponse = await http.get("https://httpbin.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/get"))
         assert(res.message.statusCode == 302, "status code should be 302");
         let body: string = await res.readBody();
     });
-    
+
     it('does basic head request', async() => {
         let res: httpm.HttpClientResponse = await _http.head('http://httpbin.org/get');
         assert(res.message.statusCode == 200, "status code should be 200");
