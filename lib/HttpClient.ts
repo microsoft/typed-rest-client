1c1b04ba4a4631dc9d352f8c208a4f112213ccda
Hamza Hejja <49674728+hamzahejja@users.noreply.github.com>
2019-12-19 17:29:21 +0200

    Integrate decoding gzip encoded response of Http Requests (#189)
    * Integrate ability to decode/process gzip encoded response of Http Requests
    
    * code style: enforce Buffer-type chunks to Buffer.concat() compatibility
    
    * code style: drop type of inner chunk const
    
    * Code Refactor with Buffer-type chunks enforcement
    
    * docs: added helper comments for Gzipped response processing logic
    
    * code style: add TS types
    
    * use utf-8 as default value for charset when decompressing gzipped content if charset not passed
    
    * Drop using default value fo charset in function to decode gzipped content
    
    * nit: optional charset which defaults to utf-8 if not passed to decoding gzip function
    
    * nit: newline at EOF
    
    * nit: sort-out readBody() function to a sole return statement for better readability
    
    * docs: add helper comments for using regex to match and obtain charset

--- lib/HttpClient.ts
+++ lib/HttpClient.ts
@@ -4,6 +4,7 @@
 import url = require("url");
 import http = require("http");
 import https = require("https");
+import util = require('./Util');
 import ifm = require('./Interfaces');
 let fs: any;
 let tunnel: any;
@@ -53,14 +54,26 @@ export class HttpClientResponse implements ifm.IHttpClientResponse {
     public message: http.IncomingMessage;
     readBody(): Promise<string> {
         return new Promise<string>(async (resolve, reject) => {
-            let output = Buffer.alloc(0);
+            let buffer: Buffer = Buffer.alloc(0);
+            const encodingCharset = util.obtainContentCharset(this);
 
-            this.message.on('data', (chunk: Buffer) => {
-                output = Buffer.concat([output, chunk]);
-            });
+            // Extract Encoding from header: 'content-encoding'
+            // Match `gzip`, `gzip, deflate` variations of GZIP encoding
+            const contentEncoding: string = this.message.headers['content-encoding'] || '';
+            const isGzippedEncoded: boolean = new RegExp('(gzip$)|(gzip, *deflate)').test(contentEncoding);
 
-            this.message.on('end', () => {
-                resolve(output.toString());
+            this.message.on('data', function(data: string|Buffer) {
+                const chunk = (typeof data === 'string') ? Buffer.from(data, encodingCharset) : data;
+                buffer = Buffer.concat([buffer, chunk]);
+            }).on('end', async function() {
+                if (isGzippedEncoded) { // Process GZipped Response Body HERE
+                    const gunzippedBody = await util.decompressGzippedContent(buffer, encodingCharset);
+                    resolve(gunzippedBody);
+                }
+
+                resolve(buffer.toString(encodingCharset));
+            }).on('error', function(err) {
+                reject(err);
             });
         });
     }
--- lib/Util.ts
+++ lib/Util.ts
@@ -4,7 +4,8 @@
 import * as qs from 'qs';
 import * as url from 'url';
 import * as path from 'path';
-import { IRequestQueryParams } from './Interfaces';
+import zlib = require('zlib');
+import { IRequestQueryParams, IHttpClientResponse } from './Interfaces';
 
 /**
  * creates an url from a request url and optional base url (http://server:8080)
@@ -75,4 +76,46 @@ function buildParamsStringifyOptions(queryParams: IRequestQueryParams): any  {
     }
 
     return options;
+}
+
+/**
+ * Decompress/Decode gzip encoded JSON
+ * Using Node.js built-in zlib module
+ *
+ * @param {Buffer} buffer
+ * @param {string} charset? - optional; defaults to 'utf-8'
+ * @return {Promise<string>}
+ */
+export async function decompressGzippedContent(buffer: Buffer, charset?: string): Promise<string> {
+    return new Promise<string>(async (resolve, reject) => {
+        zlib.gunzip(buffer, function (error, buffer) {
+            if (error) {
+                reject(error);
+            }
+
+            resolve(buffer.toString(charset || 'utf-8'));
+        });
+    })
+}
+
+/**
+ * Obtain Response's Content Charset.
+ * Through inspecting `content-type` response header.
+ * It Returns 'utf-8' if NO charset specified/matched.
+ *
+ * @param {IHttpClientResponse} response
+ * @return {string} - Content Encoding Charset; Default=utf-8
+ */
+export function obtainContentCharset (response: IHttpClientResponse) : string {
+  // Find the charset, if specified.
+  // Search for the `charset=CHARSET` string, not including `;,\r\n`
+  // Example: content-type: 'application/json;charset=utf-8'
+  // |__ matches would be ['charset=utf-8', 'utf-8', index: 18, input: 'application/json; charset=utf-8']
+  // |_____ matches[1] would have the charset :tada: , in our example it's utf-8
+  // However, if the matches Array was empty or no charset found, 'utf-8' would be returned by default.
+
+  const contentType: string = response.message.headers['content-type'] || '';
+  const matches: (RegExpMatchArray|null) = contentType.match(/charset=([^;,\r\n]+)/i);
+
+  return (matches && matches[1]) ? matches[1] : 'utf-8';
 }
