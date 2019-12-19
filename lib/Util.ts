d266d9a83c9a62fe50ee0943456986a8b06d7304
Hamza Hejja <hamzahejja@hotmail.com>
2019-12-19 17:00:47 +0200

    docs: add helper comments for using regex to match and obtain charset

--- lib/Util.ts
+++ lib/Util.ts
@@ -100,13 +100,20 @@ export async function decompressGzippedContent(buffer: Buffer, charset?: string)
 
 /**
  * Obtain Response's Content Charset.
+ * Through inspecting `content-type` response header.
+ * It Returns 'utf-8' if NO charset specified/matched.
  *
  * @param {IHttpClientResponse} response
  * @return {string} - Content Encoding Charset; Default=utf-8
  */
 export function obtainContentCharset (response: IHttpClientResponse) : string {
   // Find the charset, if specified.
-  // Match with regex "charset="
+  // Search for the `charset=CHARSET` string, not including `;,\r\n`
+  // Example: content-type: 'application/json;charset=utf-8'
+  // |__ matches would be ['charset=utf-8', 'utf-8', index: 18, input: 'application/json; charset=utf-8']
+  // |_____ matches[1] would have the charset :tada: , in our example it's utf-8
+  // However, if the matches Array was empty or no charset found, 'utf-8' would be returned by default.
+
   const contentType: string = response.message.headers['content-type'] || '';
   const matches: (RegExpMatchArray|null) = contentType.match(/charset=([^;,\r\n]+)/i);
 
