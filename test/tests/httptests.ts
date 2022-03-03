c1e30e863a9fcf20a081ee08c78aeafc487e2dc3
DaniilShmelev <v-dshmelev@microsoft.com>
2022-03-03 10:47:35 +0300

    use HTTP instead of disabling certificate validation

--- test/tests/httptests.ts
+++ test/tests/httptests.ts
@@ -10,9 +10,10 @@ import * as path from 'path';
 let sampleFilePath: string = path.join(__dirname, 'testoutput.txt');
 
 const nodeVersionsWithCertificateErrors = [6, 8];
+let redirectProtocol = 'https';
 if (nodeVersionsWithCertificateErrors.find((nodeVersion) => process.version.startsWith(`v${nodeVersion}.`))) {
-    console.log('Setting NODE_TLS_REJECT_UNAUTHORIZED = 0 to avoid certificate errors on this node version');
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    console.log('Using protocol HTTP for redirect tests to avoid certificate errors on this node version');
+    redirectProtocol = 'http';
 }
 
 describe('Http Tests', function () {
@@ -185,7 +186,7 @@ describe('Http Tests', function () {
     });
     
     it('does basic get request with redirects', async() => {
-        let res: httpm.HttpClientResponse = await _http.get("https://httpbingo.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/get"))
+        let res: httpm.HttpClientResponse = await _http.get(`${redirectProtocol}://httpbingo.org/redirect-to?url=` + encodeURIComponent("https://httpbin.org/get"))
         assert(res.message.statusCode == 200, "status code should be 200");
         let body: string = await res.readBody();
         let obj:any = JSON.parse(body);
@@ -193,7 +194,7 @@ describe('Http Tests', function () {
     });
 
     it('does basic get request with redirects (303)', async() => {
-        let res: httpm.HttpClientResponse = await _http.get("https://httpbingo.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/get") + '&status_code=303')
+        let res: httpm.HttpClientResponse = await _http.get(`${redirectProtocol}://httpbingo.org/redirect-to?url=` + encodeURIComponent("https://httpbin.org/get") + '&status_code=303')
         assert(res.message.statusCode == 200, "status code should be 200");
         let body: string = await res.readBody();
         let obj:any = JSON.parse(body);
@@ -201,14 +202,14 @@ describe('Http Tests', function () {
     });
 
     it('returns 404 for not found get request on redirect', async() => {
-        let res: httpm.HttpClientResponse = await _http.get("https://httpbingo.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/status/404") + '&status_code=303')
+        let res: httpm.HttpClientResponse = await _http.get(`${redirectProtocol}://httpbingo.org/redirect-to?url=` + encodeURIComponent("https://httpbin.org/status/404") + '&status_code=303')
         assert(res.message.statusCode == 404, "status code should be 404");
         let body: string = await res.readBody();
     });
 
     it('does not follow redirects if disabled', async() => {
         let http: httpm.HttpClient = new httpm.HttpClient('typed-test-client-tests', null, { allowRedirects: false });
-        let res: httpm.HttpClientResponse = await http.get("https://httpbingo.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/get"))
+        let res: httpm.HttpClientResponse = await http.get(`${redirectProtocol}://httpbingo.org/redirect-to?url=` + encodeURIComponent("https://httpbin.org/get"))
         assert(res.message.statusCode == 302, "status code should be 302");
         let body: string = await res.readBody();
     });
