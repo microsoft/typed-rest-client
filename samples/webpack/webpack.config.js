8d01353748a22b2a7c7805749170e2213e2f1baa
Raju Ellendula (LTIMindtree Limited) <v-ellendular@microsoft.com>
2026-06-16 17:34:13 +0530

    wip

--- samples/webpack/webpack.config.js
+++ samples/webpack/webpack.config.js
@@ -9,10 +9,6 @@ const config = {
         main: "./main.js",
     },
     resolve: {
-        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-        alias: {
-            'process/browser': require.resolve('process/browser')
-        },
         fallback: {
             fs: false,
             "http": require.resolve("stream-http"),
@@ -23,9 +19,7 @@ const config = {
             "assert": require.resolve("assert"),
             "util": require.resolve("util"),
             "stream": require.resolve("stream-browserify"),
-            "buffer": require.resolve("buffer"),
-            "events": require.resolve("events/"),
-            "process": require.resolve("process/browser")
+            "buffer": require.resolve("buffer")
         }
     },
     output: {
